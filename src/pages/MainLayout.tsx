badb5cea32b25806a28b5080129ce64f1fcc78ce
anton <kamaeff2@gmail.com>
2025-04-13 15:10:15 +0300

    добавил начальный адаптив и поправил выделение селектора

--- src/pages/MainLayout.tsx
+++ src/pages/MainLayout.tsx
@@ -22,11 +22,14 @@ const MainLayout: React.FC = () => {
       <div className='bg'>
         <div className='h-[10vh] flex items-center justify-end me-20 gap-4 uppercase'>
           <nav>
-            <ul className='flex items-center gap-2 bg-white text-black pt-3 px-2 tracking-[-2px] text-[20px]'>
+            <ul className='flex flex-col sm:flex-row items-center gap-2 bg-white text-black p-2 tracking-[-2px] '>
               {list.map(({titleKey, link}, i: number) => (
                 <li key={i} className='hover:underline leading-none'>
                   <Hover scale={1.02}>
-                    <Link to={link} className='font-bold'>
+                    <Link
+                      to={link}
+                      className='font-bold text-[16px] sm:text-[20px]'
+                    >
                       {t(titleKey)}
                     </Link>
                   </Hover>
@@ -38,24 +41,21 @@ const MainLayout: React.FC = () => {
           <LanguageSwitcher />
         </div>
 
-        <div className='h-[90vh] flex flex-col justify-between items-center select-none'>
-          <FadeIn className='w-full flex flex-col text-[55px] md:px-20 px-40 py-0 flex-grow justify-center'>
+        <div className='h-[90vh] flex flex-col justify-between items-start select-none'>
+          <FadeIn className='flex flex-col text-[30px] sm:text-[55px] md:px-20 px-10 py-0 flex-grow justify-center'>
             <h2 className='leading-none'>
               <Link
                 to='/register'
-                className='group tracking-[-8px] uppercase font-bold'
+                className='group tracking-[-3px] sm:tracking-[-8px] uppercase font-bold'
               >
-                <Flex
-                  gap={'5'}
-                  className='transition-all duration-200 hover:text-[#C3073F]'
-                >
+                <Flex className='transition-all duration-200 hover:text-[#C3073F] gap-2 md:gap-5 flex-wrap leading-7'>
                   <span className='group-hover:tracking-[-4px] transition-all duration-200'>
                     {t('main.connect.first')}
                   </span>
                   <span className='group-hover:tracking-[0px] transition-all duration-200'>
                     {t('main.connect.second')}
                   </span>
-                  <span className='group-hover:tracking-[2px] transition-all duration-200'>
+                  <span className='group-hover:tracking-[2px] transition-all duration-200 break-words sm:w-auto w-full'>
                     {t('main.connect.third')}
                   </span>
                 </Flex>
@@ -70,12 +70,9 @@ const MainLayout: React.FC = () => {
               <h2 className='leading-none'>
                 <Link
                   to='/register'
-                  className='group tracking-[-8px] uppercase font-bold'
+                  className='group tracking-[-2px] sm:tracking-[-8px] uppercase font-bold'
                 >
-                  <Flex
-                    gap={'5'}
-                    className='transition-all duration-200 hover:text-[#C3073F]'
-                  >
+                  <Flex className='transition-all duration-200 hover:text-[#C3073F] gap-1 md:gap-5'>
                     <span className='group-hover:tracking-[-4px] transition-all duration-200'>
                       {t('main.book.first')}
                     </span>
@@ -92,7 +89,7 @@ const MainLayout: React.FC = () => {
           </FadeIn>
 
           <button
-            className='mb-4 text-[20px] px-6 font-normal uppercase border border-white'
+            className='mb-4 mx-auto text-[20px] px-6 font-normal uppercase border border-white transition-all duration-200 hover:text-[#C3073F] hover:border-[#C3073F]'
             onClick={scrollToNextSection}
           >
             {t('main.about')}
@@ -100,9 +97,9 @@ const MainLayout: React.FC = () => {
         </div>
       </div>
 
-      {/* <div ref={nextSectionRef} className='h-[95vh]'>
+      <div ref={nextSectionRef} className='h-screen'>
         <div>Это следующий контейнер</div>
-      </div> */}
+      </div>
     </div>
   );
 };
